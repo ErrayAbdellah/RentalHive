66da879848f4d5246b0114e333fc9c31a98458dd
ErrayAbdellah <mahdiabdllah121@gmail.com>
2023-11-14 00:24:07 +0100

    first commit

---
+++ src/main/java/com/rentalHive/rentalHive/Hello.java
@@ -0,0 +1,12 @@
+package com.rentalHive.rentalHive;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
