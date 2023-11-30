559421d7d1e6369cbf4091054f9597fba0d1cb13
Boutaina <boutaina.elatbaoui@gmail.com>
2023-11-30 11:11:20 +0100

    Added the validation of demande

--- src/main/java/com/rentalHive/rentalHive/controller/DemandeController.java
+++ src/main/java/com/rentalHive/rentalHive/controller/DemandeController.java
@@ -1,8 +1,11 @@
 package com.rentalHive.rentalHive.controller;
 
+import com.rentalHive.rentalHive.enums.State;
 import com.rentalHive.rentalHive.model.dto.DemandeDTO;
 import com.rentalHive.rentalHive.model.entities.Demande;
 import com.rentalHive.rentalHive.service.IDemandeService;
+import jakarta.annotation.Nullable;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,12 +23,12 @@ public class DemandeController {
     }
 
     @GetMapping("/all")
-    public ResponseEntity<List<Demande>> getAllDemandes() {
-        return demandeService.getAllDemandes();
+    public ResponseEntity<List<Demande>> getAllDemandes(@RequestParam(required = false)State state) {
+        return demandeService.getAllDemandes(state);
     }
 
     @PostMapping("/create")
-    public ResponseEntity<String> createDemande(@RequestBody DemandeDTO demandeDTO) {
+    public ResponseEntity<String> createDemande(@Valid @RequestBody DemandeDTO demandeDTO) {
         return demandeService.createDemande(demandeDTO);
     }
 }
--- src/main/java/com/rentalHive/rentalHive/repository/IDemandeRepo.java
+++ src/main/java/com/rentalHive/rentalHive/repository/IDemandeRepo.java
@@ -1,10 +1,32 @@
 package com.rentalHive.rentalHive.repository;
 
+import com.rentalHive.rentalHive.enums.Category;
+import com.rentalHive.rentalHive.enums.State;
+import com.rentalHive.rentalHive.model.dto.DemandeDTO;
+import com.rentalHive.rentalHive.model.dto.EquipmentDTO;
 import com.rentalHive.rentalHive.model.entities.Demande;
+import com.rentalHive.rentalHive.model.entities.Equipment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
 
 @Repository
 public interface IDemandeRepo extends JpaRepository<Demande, Long> {
+    List<Demande> findDemandeByState(State state);
+
+    @Query("SELECT d FROM Demande d " +
+            "JOIN d.equipment e " +
+            "WHERE e.equipmentId IN :equipmentIds " +
+            "AND ((:demandeDate BETWEEN d.demande_date AND d.date_retour) OR " +
+            "(:dateRetour BETWEEN d.demande_date AND d.date_retour))")
+    List<Demande> findExistingReservations(
+            @Param("equipmentIds") List<Long> equipmentIds,
+            @Param("demandeDate") Date demandeDate,
+            @Param("dateRetour") Date dateRetour
+    );
 
 }
